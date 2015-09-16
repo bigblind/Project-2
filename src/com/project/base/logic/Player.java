023e080852df318f4ac5adbed3b03d8ab29d04ce
Leonardo Colacicchi <leonardocolacicchi@gmail.com>
2015-09-16 16:30:19 +0200

    player class

--- src/com/project/base/logic/Player.java
+++ src/com/project/base/logic/Player.java
@@ -1,5 +1,15 @@
 package com.project.base.logic;
 
 public class Player {
-
+  
+    private int numbOfStones;
+    private String name;
+    
+    public Player(String name){
+	this.name = name;
+    }
+    
+    public int getNumbOfStones(){
+	return this.numbOfStones;
+    }
 }
