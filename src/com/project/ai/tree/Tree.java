72fbc6516d00fd9c449691bb708e2244ccbef34a
bastianbertram <bastianbertram@bastians-mbp.wireless.unimaas.local>
2015-10-30 10:25:38 +0100

    Some changes

--- src/com/project/ai/tree/Tree.java
+++ src/com/project/ai/tree/Tree.java
@@ -1,5 +1,67 @@
 package com.project.ai.tree;
 
-public class Tree {
+import java.util.ListIterator;
+
+public class Tree<E> implements TreeADT {
+
+	public Object root() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object parent(Object node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ListIterator children(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isInternal(Object node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isExternal(Object node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isRoot(Object node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void swapElements(Object firstNode, Object secondNode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object replaceElement(Object oldNode, Object newElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ListIterator elements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ListIterator positions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
