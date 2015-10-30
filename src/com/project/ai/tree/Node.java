ec9f6ac95f044a55b3444c32eec3788173046f5e
bastianbertram <bastianbertram@bastians-mbp.wireless.unimaas.local>
2015-10-30 15:10:01 +0100

    Added the board to the node

--- src/com/project/ai/tree/Node.java
+++ src/com/project/ai/tree/Node.java
@@ -2,6 +2,8 @@ package com.project.ai.tree;
 
 import java.util.ArrayList;
 
+import com.project.server.logic.board.Board;
+
 
 public class Node<E> implements PositionADT<E>{
 
@@ -9,31 +11,35 @@ public class Node<E> implements PositionADT<E>{
 		private E pathCost;
 		private E estimateCost;
 		private ArrayList<Node> children;
+		private Board board;
 		
 		
 		
-		public Node(E pathCost){
+		public Node(E pathCost, Board board){
 			this.parent = null;
 			this.pathCost = pathCost;
 			this.estimateCost = null;
+			this.board = board;
 			this.children = null;
 		}
 		
 		
 		
-		public Node(Node parent, E pathCost){
+		public Node(Node parent, E pathCost, Board board){
 			this.parent = parent;
 			this.pathCost = pathCost;
 			this.estimateCost = null;
+			this.board = board;
 			this.children = null;
 		}
 		
 		
 		
-		public Node(Node parent, E pathCost, E estimateCost, ArrayList<Node> children){
+		public Node(Node parent, E pathCost, E estimateCost, Board board, ArrayList<Node> children){
 			this.parent = parent;
 			this.pathCost = pathCost;
 			this.estimateCost = estimateCost;
+			this.board = board;
 			this.children = children;
 		}
 		
